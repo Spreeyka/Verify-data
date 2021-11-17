b1d6a709cb7ba4c71a7a7415a2af0b5d1f0321d0
Dominik Pietrzyca <defres15@gmail.com>
2021-11-17 19:50:33 +0100

    add more API links to table

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -5,9 +5,9 @@
 // CHART_3_CONSTANTS - % of numbers from 0-9 occuring on second significant position, where first significant position number equals 1
 
 export const CONSTANTS = {
-  CHART_0_CONSTANTS: [30.1, 17.7, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6],
-  CHART_1_CONSTANTS: [11.9, 11.3, 10.9, 10.4, 10.0, 9.7, 9.3, 9.0, 8.8, 8.6],
-  CHART_2_CONSTANTS: [10.2, 10.1, 10.0, 10.0, 10.0, 9.9, 9.9, 9.9, 9.9, 9.8],
+  CHART_0_CONSTANTS: [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6],
+  CHART_1_CONSTANTS: [12.0, 11.4, 10.9, 10.4, 10.0, 9.7, 9.3, 9.0, 8.8, 8.5],
+  CHART_2_CONSTANTS: [10.2, 10.1, 10.1, 10.1, 10.0, 10.0, 9.9, 9.9, 9.9, 9.8],
   CHART_3_CONSTANTS: [13.7, 12.5, 11.5, 10.7, 9.9, 9.3, 8.7, 8.2, 7.8, 7.4],
 };
 
@@ -38,6 +38,11 @@ export const CORRELATION_CRITICAL_VALUES = {
 
 export const API = [
   `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/globalne/plec_g.json`,
-  `https://123.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/globalne/plec_g.json`,
-  `https://456.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/globalne/plec_g.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/globalne/1.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/globalne/zamieszkanie_g.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/krajowe/zamieszkanie_k.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/custodian/desa-unsd.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/custodian/ilo.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/custodian/icao.json`,
+  `https://raw.githubusercontent.com/statisticspoland/sdg-indicators-pl/master/api/v1/custodian/fao.json`,
 ];
